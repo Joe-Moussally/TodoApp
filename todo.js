449d49c1fb9dc36715fef06c07a36e8840b8ac56
MJoe111 <joemoussally111@gmail.com>
2022-05-28 21:22:31 +0300

    Adding prepend

--- todo.js
+++ todo.js
@@ -1,16 +1,6 @@
 const input = document.getElementsByTagName("input")[0];
 const check = document.getElementsByClassName("fa-check");
 
-//adding event listeners to check buttons
-// $('.fa-check').click((e) => {
-//      handleCheck(e.target);
-//  })
-
-//  //adding event listeners to delete buttons
-// $('.fa-trash-can').click((e) => {
-//     handleDelete(e.target);
-// })
-
 //adding event listener to text input
 $("input").on('keypress',function(e) {
     if(e.which == 13) {
@@ -22,7 +12,7 @@ $("input").on('keypress',function(e) {
 
 //functions that handles on ENTER press to append to li
 const handleKeyPress = (value) => {
-    if (value === '') {
+    if (value == '') {
         return
     } else {
         addTodo(value);
@@ -43,5 +33,5 @@ const handleCheck = (button) => {
 
 //function that appends the Todo content to ul tag
 const addTodo = (todo) => {
-    $('#todo-list-ul').append('<li><span>'+todo+'</span><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
+    $('#todo-list-ul').prepend('<li><span>'+todo+'</span><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
 }
