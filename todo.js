1765dfe0947610930972af39e6246d29da9b0976
MJoe111 <joemoussally111@gmail.com>
2022-05-28 20:31:04 +0300

    Adding of event listeners to delete and check

--- todo.js
+++ todo.js
@@ -1,7 +1,37 @@
 const input = document.getElementsByTagName("input")[0];
+const check = document.getElementsByClassName("fa-check");
 
+//adding event listeners to check buttons
+$('.fa-check').on('click', (e) => {
+     console.log('Clicked!',e.target);
+     handleCheck(e.target);
+ })
+
+ //adding event listeners to delete buttons
+$('.fa-trash-can').on('click', () => {
+    console.log('Clicked!');
+    handleDelete(e.target);
+})
+
+
+//adding event listener to text input
 $("input").on('keypress',function(e) {
     if(e.which == 13) {
         console.log(e,e.currentTarget.value)
+        e.currentTarget.value = '';//clear input value
     }
 });
+
+//functions that handles on ENTER press to append to li
+const handleKeyPress = (value) => {
+    if (value == '') {
+        return
+    } else {
+        addTodo(value);
+    }
+}
+
+//function that appends the Todo content to ul tag
+const addTodo = (todo) => {
+
+}
