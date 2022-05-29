59bbd34a5876ef50e5cd348ce700c877227693e2
MJoe111 <joemoussally111@gmail.com>
2022-05-29 13:23:21 +0300

    Fixing refresh bug

--- todo.js
+++ todo.js
@@ -2,12 +2,13 @@ const input = document.getElementsByTagName("input")[0];
 const check = document.getElementsByClassName("fa-check");
 const list = document.getElementById("todo-list-ul");
 const done = document.getElementById("todo-list-done");
-let todoList = JSON.parse(localStorage.getItem('todos'));
 
-function todo(task,point) {
+
+function todo(task,point,done) {
 
     this.task = task;
     this.point = point;
+    this.done = done;
 }
 
 //adding event listener to text input
@@ -30,6 +31,19 @@ const handleKeyPress = (value) => {
 
 //function that handles the delete buttons click
 const handleDelete = (button) => {
+    let todoList = JSON.parse(localStorage.getItem('todos'));
+    let taskName = $(button).prev().prev().prev().html();
+    console.log(taskName);
+     for (i = 0; i < todoList.length-1; i++) {
+         if (todoList[i].task == taskName) {
+            todoList.pop(i)
+            console.log("Popped")
+         }
+     }
+
+     localStorage.setItem('todos',JSON.stringify(todoList));
+     
+
     $(button).parent().fadeOut(150)
 }
 
@@ -81,17 +95,39 @@ const handleCheck = (button) => {
 
 //function that appends the Todo content to ul tag
 const addTodo = (task) => {
-    let temp = new todo(task,5);
+    let todoList = JSON.parse(localStorage.getItem('todos'));
+    let temp = new todo(task,5,false);
 
     if (todoList === null) {
         todoArray = [];
     } else {
         todoArray = todoList;
     }
+    
     todoArray.push(temp);
     console.log(todoArray)
     localStorage.setItem('todos',JSON.stringify(todoArray))
-
+    refresh();
     
     // $('#todo-list-ul').prepend('<li><span contentEditable="false">'+temp.task+'</span><i class="fa-solid fa-pen" onclick="handleEdit(event.currentTarget)"></i><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
 }
+
+//function that updates the list and displays it
+const refresh = () => {
+    let todoList = JSON.parse(localStorage.getItem('todos'));
+
+    if (todoList === null) {
+        todoArray = [];
+    } else {
+        todoArray = todoList;
+    }
+
+    $('#todo-list-ul').empty();
+
+    todoArray.forEach((todo) => {
+        $('#todo-list-ul').prepend('<li><span contentEditable="false">'+todo.task+'</span><i class="fa-solid fa-pen" onclick="handleEdit(event.currentTarget)"></i><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
+    })
+}
+
+refresh()
+
