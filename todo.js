f723f6871fd4af12462ebac96552bcc78ddaacd2
MJoe111 <joemoussally111@gmail.com>
2022-05-29 12:32:28 +0300

    Adding JSON to setItem

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const input = document.getElementsByTagName("input")[0];
 const check = document.getElementsByClassName("fa-check");
 const list = document.getElementById("todo-list-ul");
 const done = document.getElementById("todo-list-done");
+let todoList = JSON.parse(localStorage.getItem('todos'));
 
 function todo(task,point) {
 
@@ -80,21 +81,17 @@ const handleCheck = (button) => {
 
 //function that appends the Todo content to ul tag
 const addTodo = (task) => {
-
     let temp = new todo(task,5);
-    let x = localStorage.getItem('todos');
-    $(x).prepend(temp);
-    localStorage.setItem('todos',x)
-    console.log(localStorage.getItem('todos'))
-    $('#todo-list-ul').prepend('<li><span contentEditable="false">'+temp.task+'</span><i class="fa-solid fa-pen" onclick="handleEdit(event.currentTarget)"></i><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
+
+    if (todoList === null) {
+        todoArray = [];
+    } else {
+        todoArray = todoList;
+    }
+    todoArray.push(temp);
+    console.log(todoArray)
+    localStorage.setItem('todos',JSON.stringify(todoArray))
+
+    
+    // $('#todo-list-ul').prepend('<li><span contentEditable="false">'+temp.task+'</span><i class="fa-solid fa-pen" onclick="handleEdit(event.currentTarget)"></i><i class="fa-solid fa-check" onclick="handleCheck(event.target)"></i><i class="fa-solid fa-trash-can" onclick="handleDelete(event.target)"></i></li>')
 }
-
-//function that refreshes the list on any modification and refresh
-//update()
-
-if (localStorage.getItem('todos') === null || localStorage.getItem('todos') === 'null') {
-    localStorage.setItem('todos',[])
-    console.log("SET")
-}
-
-console.log(localStorage.getItem('todos'))
